699e11e1feb27f3e38a87dd41f3cc55929745070
Park Seong Tae <sungkae94@gmail.com>
2018-10-30 16:09:10 +0900

    오류해결

--- 단계별로 풀어보기/1924_2007년/2018_10_30.cpp
+++ 단계별로 풀어보기/1924_2007년/2018_10_30.cpp
@@ -6,17 +6,16 @@ int main()
 {
 	const int lastDay[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
 	const string week[7] = { "MON","TUE","WED","THU","FRI","SAT","SUN" };
-	
-	int month, day, answer=0;
-	cin >> month >>day;
+
+	int month, day, answer = 0;
+	cin >> month >> day;
 
 	for (int i = 1; i < month; i++)
 	{
-		answer += lastDay[i-1];
+		answer += lastDay[i - 1];
 	}
 	answer += day;
-	string result = week[(answer-1)%7];
+	string result = week[(answer - 1) % 7];
 	cout << result << endl;
-	system("pause");
 	return 0;
 }
